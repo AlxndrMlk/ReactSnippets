c9a84e85fe2ed255bca11445e2dbd6f69b7b5ea1
Aleksander Molak <aleksander.molak@gmail.com>
2018-07-16 22:24:40 +0200

    Update reactForm.js

--- reactForm.js
+++ reactForm.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class Input extends React.Component {
+export default class Input extends React.Component {
   constructor(props) {
     super(props);
     
